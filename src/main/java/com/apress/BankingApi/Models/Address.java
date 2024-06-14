1656e0012b4d6ad97b05dd1c18c0118bdf4c26da
JRThompson <JRThompson@wil.yearup.org>
2024-06-14 14:13:23 -0400

    adres

--- src/main/java/com/apress/BankingApi/Models/Address.java
+++ src/main/java/com/apress/BankingApi/Models/Address.java
@@ -1,6 +1,22 @@
 package com.apress.BankingApi.Models;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Address
 {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ADDRESS_ID")
+    private long id;
+    @Column(name="ADDRESS_NUMBER")
+    private String street_Number;
+    @Column(name="STREET_NAME")
+    private String street_Name;
+    @Column(name="CITY")
+    private String city;
+    @Column(name="STATE")
+    private String state;
+    @Column(name="ZIP")
+    private String zip;
 }
