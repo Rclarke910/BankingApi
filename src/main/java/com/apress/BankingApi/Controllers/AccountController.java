be1abdb97f5b0b3b117c9585fda9abe0657d2993
Rashawn <Rashawnclarke1998@gmail.com>
2024-06-18 11:59:24 -0400

    fixed account repo

--- src/main/java/com/apress/BankingApi/Controllers/AccountController.java
+++ src/main/java/com/apress/BankingApi/Controllers/AccountController.java
@@ -1,4 +1,47 @@
 package com.apress.BankingApi.Controllers;
 
+import com.apress.BankingApi.Models.Account;
+import com.apress.BankingApi.Services.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class AccountController {
+
+    @Autowired
+    private AccountService accountService;
+
+    @GetMapping("/accounts")
+    public Iterable<Account> getAllAccounts() { return accountService.getAllAccounts(); }
+
+    @GetMapping("/accounts/{accountId}")
+    public Optional<Account> getAccountById(@PathVariable int accountId) { return accountService.getAccountById(accountId); }
+
+    @GetMapping("/customers/{customerid}/accounts")
+    public List<Account> getAccountsByCustomerId(@PathVariable long customerid){
+        List<Account> accounts = accountService.getAccountsByCustomerId(customerid);
+        return accounts;
+    }
+
+    @PostMapping("customers/{customerId}/accounts")
+    public void addAccount(@RequestBody Account account, @PathVariable long customerId){
+        accountService.createAccount(account,customerId);
+    }
+
+    @PutMapping("/accounts/{accountId}")
+    public void updateAccount(@PathVariable int accountId, @RequestBody Account account){
+        accountService.updateAccount(accountId,account);
+    }
+
+    @DeleteMapping("/account/{accountId}")
+    public void deleteAccount(@PathVariable long id){
+        accountService.deleteAccount(id);
+    }
+
+
 }
--- src/main/java/com/apress/BankingApi/Models/Account.java
+++ src/main/java/com/apress/BankingApi/Models/Account.java
@@ -19,3 +19,52 @@ public class Account {
     @ManyToOne
     @JoinColumn(name = "customer_id")
     private Customer customer;
+
+    public Integer getRewards() {
+        return rewards;
+    }
+
+    public void setRewards(Integer rewards) {
+        this.rewards = rewards;
+    }
+
+    public AccountType getType() {
+        return type;
+    }
+
+    public void setType(AccountType type) {
+        this.type = type;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
