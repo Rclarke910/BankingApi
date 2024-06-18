d4f42df6af0668033ed31f806ad035b181ba9cd9
JRThompson <JRThompson@wil.yearup.org>
2024-06-18 13:55:04 -0400

    Adress geter

--- src/main/java/com/apress/BankingApi/Models/Address.java
+++ src/main/java/com/apress/BankingApi/Models/Address.java
@@ -20,4 +20,47 @@ public class Address
     private String state;
     @Column(name="ZIP")
     private String zip;
+
+    public long getId() {
+        return id;
+    }
+    public String getStreet_Number() {
+        return street_Number;
+    }
+
+    public void setStreet_Number(String street_Number) {
+        this.street_Number = street_Number;
+    }
+
+    public String getStreet_Name() {
+        return street_Name;
+    }
+
+    public void setStreet_Name(String street_Name) {
+        this.street_Name = street_Name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
 }
