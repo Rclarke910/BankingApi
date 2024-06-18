3d0204d7dd6b46448b68c06072ea427269736be1
gwyn <gwynethmojados@gmail.com>
2024-06-18 12:02:21 -0400

    push

---
+++ src/main/java/com/apress/BankingApi/Response/BillResponse.java
@@ -0,0 +1,15 @@
+package com.apress.BankingApi.Response;
+
+public class BillResponse {
+//    public ResponseEntity<?> getBillByAccount(Long accountId) throws Exception {
+//        Iterable<Bill> bill1 = billService.getBillByAccount(accountId);
+//
+//        Body body = new Body();
+//        body.setMessage("Bills for account " + accountId + " retrieved");
+//        body.setData(billService.getBills());
+//        body.setCode(HttpStatus.OK.value());
+//        return new ResponseEntity<>(body, HttpStatus.OK);
+//    }
+
+
+}
