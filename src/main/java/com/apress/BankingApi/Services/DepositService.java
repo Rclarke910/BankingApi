c3e75d2b01d2b84a97b24bfd50f73b0dda6f2537
Alvmo1 <moralesalv101@hotmail.com>
2024-06-18 20:03:50 -0400

    push12

--- src/main/java/com/apress/BankingApi/Services/DepositService.java
+++ src/main/java/com/apress/BankingApi/Services/DepositService.java
@@ -1,6 +1,7 @@
 package com.apress.BankingApi.Services;
 
 
+import com.apress.BankingApi.Exception.ResourceNotFoundException;
 import com.apress.BankingApi.Exceptions.CustomerNotFoundException;
 import com.apress.BankingApi.Models.Deposit;
 import com.apress.BankingApi.Repos.AccountRepository;
@@ -16,36 +17,56 @@ import java.util.Optional;
     @Service
     public class DepositService {
 
-
         @Autowired
         private DepositRepository depositRepository;
+        @Autowired
+        private AccountRepository accountRepository;
 
-
-        public void saveDeposit(Deposit deposit) {
-            depositRepository.save(deposit);
-        }
-
-        public void deleteDeposit(Deposit deposit) {
-            depositRepository.delete(deposit);
-        }
-
-        public Iterable<Deposit> getAllDeposits() {
-            return depositRepository.findAll();
-        }
-
-        public Optional<Deposit> getDepositById(Long id) {
-            return depositRepository.findById(id);
-        }
-
-        public void updateDeposit(Long id, Deposit deposit) {
-            for (Deposit d : getAllDeposits()) {
-                if (d.getId().equals(id)) {
-                    depositRepository.save(deposit);
-                }
+        // Verify
+        public void verifyDepositById(Long id) {
+            if (!depositRepository.findById(id).isPresent()) {
+                throw new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND));
             }
         }
+
+        public void verifyAccountById(Long id) {
+            if (!accountRepository.findById(id).isPresent()) {
+                throw new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND));
+            }
+        }
+
+        // Create
+        public Deposit createDeposit(Deposit deposit, Long accountId) {
+            verifyAccountById(accountId);
+            return depositRepository.save(deposit);
+        }
+
+        // Get
+        public List<Deposit> getDepositsByAccountId(Long accountId) {
+            verifyAccountById(accountId);
+            return depositRepository.findDepositById(accountId);
+        }
+
+        // Get By Account ID
+        public Deposit getDepositById(Long id) {
+            verifyDepositById(id);
+            return depositRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND)));
+        }
+
+        // Update
+        public Deposit updateDeposit(Deposit deposit) {
+            verifyDepositById(deposit.getId());
+            Deposit oldDeposit = getDepositById(deposit.getId());
+            deposit.setId(oldDeposit.getId());
+            deposit.setPayeeId(oldDeposit.getPayeeId());
+            return depositRepository.save(deposit);
+        }
+
+        // Delete
+        public void deleteDepositById(Long id) {
+            verifyDepositById(id);
+            depositRepository.deleteById(id);
+        }
     }
-
-
-
 
