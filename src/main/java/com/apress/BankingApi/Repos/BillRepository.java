a2be830f6f47f0f4b1154f23c8fbfee4f39a319c
gwyn <gwynethmojados@gmail.com>
2024-06-20 15:48:45 -0400

    bills

--- src/main/java/com/apress/BankingApi/Repos/BillRepository.java
+++ src/main/java/com/apress/BankingApi/Repos/BillRepository.java
@@ -9,9 +9,11 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface BillRepository extends JpaRepository<Bill, Long> {
-    @Query(value = "SELECT b FROM Bill b WHERE b.account_id = :accountId",nativeQuery = true)
-    List<Bill> findByAccountId(@Param("accountId") Long accountId);
-
+    @Query(value="select b.* from Account a, Bill b where a.ID = ?1 and b.ACCOUNT_ID = a.ID", nativeQuery = true)
+   // @Query(value = "SELECT b FROM Bill b WHERE b.account_id = :accountId",nativeQuery = true)
+    List<Bill> findByAccountId( Long accountId);
+//    @Query(value="select b.* from Account a, Bill b where a.CUSTOMER_ID = ?1 and b.ACCOUNT_ID = a.ACCOUNT_ID", nativeQuery = true)
+//    List<Bill> findByCustomerId (Long customerId) ;
 }
 
 
