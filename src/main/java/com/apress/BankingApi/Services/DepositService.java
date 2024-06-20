46e3f8b48af4d3b79d01c04e21416517ef951920
Alvmo1 <moralesalv101@hotmail.com>
2024-06-20 09:59:22 -0400

    push14

--- src/main/java/com/apress/BankingApi/Services/DepositService.java
+++ src/main/java/com/apress/BankingApi/Services/DepositService.java
@@ -14,64 +14,63 @@ import java.util.List;
 @Service
     public class DepositService {
 
-        @Autowired
-        private DepositRepository depositRepository;
-        @Autowired
-        private AccountRepository accountRepository;
+    @Autowired
+    private DepositRepository depositRepository;
+    @Autowired
+    private AccountRepository accountRepository;
 
-        @Autowired
-        private DepositService depositService;
 
-        // Verify
-        public void verifyDepositById(Long id) {
-            if (!depositRepository.findById(id).isPresent()) {
-                throw new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND));
-            }
+    // Verify
+    public void verifyDepositById(Long id) {
+        if (!depositRepository.findById(id).isPresent()) {
+            throw new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND));
         }
+    }
 
-        public void verifyAccountById(Long id) {
-            if (!accountRepository.findById(id).isPresent()) {
-                throw new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND));
-            }
+    public void verifyAccountById(Long id) {
+        if (!accountRepository.findById(id).isPresent()) {
+            throw new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND));
         }
+    }
 
-        // Create
-        public Deposit createDeposit(Deposit deposit) {
-            verifyAccountById(deposit.getPayeeId());
-            return depositRepository.save(deposit);
-        }
+    // Create
+    public Deposit createDeposit(Deposit deposit) {
+        verifyAccountById(deposit.getPayeeId());
+        return depositRepository.save(deposit);
+    }
 
-        // Get
-        public List<Deposit> getDepositsByAccountId(Long accountId) {
-            verifyAccountById(accountId);
-            return depositRepository.findDepositById(accountId);
-        }
+    // Get
+    public List<Deposit> getDepositsByAccountId(Long accountId) {
+        verifyAccountById(accountId);
+        return depositRepository.findDepositById(accountId);
+    }
 
-        // Get By Account ID
-        public Deposit getDepositById(Long id) {
-            verifyDepositById(id);
-            return depositRepository.findById(id)
-                    .orElseThrow(() -> new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND)));
-        }
+    // Get By Account ID
+    public Deposit getDepositById(Long id) {
+        verifyDepositById(id);
+        return depositRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.valueOf(HttpStatus.NOT_FOUND)));
+    }
 
-        // Update
-        public Deposit updateDeposit(Deposit deposit) {
-            verifyDepositById(deposit.getId());
-            Deposit oldDeposit = getDepositById(deposit.getId());
-            deposit.setId(oldDeposit.getId());
-            deposit.setPayeeId(oldDeposit.getPayeeId());
-            return depositRepository.save(deposit);
-        }
+    // Update
+    public Deposit updateDeposit(Deposit deposit) {
+        verifyDepositById(deposit.getId());
+        Deposit oldDeposit = getDepositById(deposit.getId());
+        deposit.setId(oldDeposit.getId());
+        deposit.setPayeeId(oldDeposit.getPayeeId());
+        return depositRepository.save(deposit);
+    }
 
-        // Delete
-        public void deleteDepositById(Long id) {
-            verifyDepositById(id);
-            depositRepository.deleteById(id);
-        }
-
-    // Get All Deposits
-    public List<Deposit> getAllDeposits() {
-        return depositService.getAllDeposits();
+    // Delete
+    public void deleteDepositById(Long id) {
+        verifyDepositById(id);
+        depositRepository.deleteById(id);
     }
 }
+
+//    // Get All Deposits
+//    public List<Deposit> getAllDeposits() {
+//        return depositRepository.getAllDeposits();
+//    }
+//}
 
