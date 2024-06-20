5bd8d7559199deaf501911abf77f69f159490349
gwyn <gwynethmojados@gmail.com>
2024-06-19 20:00:44 -0400

    bill response

--- src/main/java/com/apress/BankingApi/Response/BillResponse.java
+++ src/main/java/com/apress/BankingApi/Response/BillResponse.java
@@ -1,15 +1,138 @@
 package com.apress.BankingApi.Response;
 
+import com.apress.BankingApi.Exception.ResourceNotFoundException;
+import com.apress.BankingApi.Models.Bill;
+import com.apress.BankingApi.Services.BillService;
+import com.apress.BankingApi.dto.Body;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Optional;
+
 public class BillResponse {
-//    public ResponseEntity<?> getBillByAccount(Long accountId) throws Exception {
-//        Iterable<Bill> bill1 = billService.getBillByAccount(accountId);
-//
-//        Body body = new Body();
-//        body.setMessage("Bills for account " + accountId + " retrieved");
-//        body.setData(billService.getBills());
-//        body.setCode(HttpStatus.OK.value());
-//        return new ResponseEntity<>(body, HttpStatus.OK);
-//    }
+    @Autowired
+    BillService billservice;
+
+    public ResponseEntity<?> createBill(Bill bill) {
+        try {
+            Bill createdBill = billservice.createBill(bill);
+
+            Body body = new Body();
+            body.setData(createdBill);
+            body.setCode(HttpStatus.CREATED.value());
+            body.setMessage("Bill created Successfully");
+
+            return ResponseEntity.ok(body);
+        } catch (Exception exception) {
+            Body body = new Body();
+            body.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            body.setMessage("Error Posting Bill");
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+
+        }
+    }
+
+    public ResponseEntity<?> getAllBills() {
+        try {
+            Body body = new Body();
+            body.setData(billservice.getAllBills());
+            body.setCode(HttpStatus.OK.value());
+            body.setMessage("Successfully retrieved Bills");
+
+            return new ResponseEntity<>(body, HttpStatus.OK);
+        } catch (Exception exception) {
+            Body body = new Body();
+            body.setCode(HttpStatus.NOT_FOUND.value());
+            body.setMessage("Error fetching Bill");
+
+            return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        }
+    }
+
+    public ResponseEntity<?> getBillById(Long billId) {
+        try {
+            Optional<Bill> bill = billservice.getBillById(billId);
+
+            if (bill.isPresent()) {
+                Body body = new Body();
+                body.setData(bill.get());
+                body.setCode(HttpStatus.OK.value());
+                body.setMessage("Bill retrieval success");
+
+                return new ResponseEntity<>(body, HttpStatus.OK);
+            } else {
+                Body body = new Body();
+                body.setCode(HttpStatus.NOT_FOUND.value());
+                body.setMessage("Bill not found");
+
+                return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+            }
+        } catch (Exception e) {
+            Body body = new Body();
+            body.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            body.setMessage("Error finding Bill");
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<?> updateBill(Bill updatedBill, Long billId) {
+        try {
+            Bill editedBill = billservice.updateBill(updatedBill, billId);
+
+            if (editedBill != null) {
+                Body body = new Body();
+                body.setData(editedBill);
+                body.setCode(HttpStatus.OK.value());
+                body.setMessage("Bill updated successfully");
+
+                return new ResponseEntity<>(body, HttpStatus.OK);
+            } else {
+                Body body = new Body();
+                body.setCode(HttpStatus.NOT_FOUND.value());
+                body.setMessage("Customer not found");
+
+                return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+            }
+        } catch (ResourceNotFoundException e) {
+            Body body = new Body();
+            body.setCode(HttpStatus.NOT_FOUND.value());
+            body.setMessage(e.getMessage());
+
+            return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            Body body = new Body();
+            body.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            body.setMessage("Error updating Bill");
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    public ResponseEntity<?> deleteBill(Long billId) {
+        try {
+            billservice.deleteBillById(billId);
+            Body body = new Body();
+            body.setCode(HttpStatus.NO_CONTENT.value());
+            body.setMessage("Bill deleted successfully");
+
+            return new ResponseEntity<>(body, HttpStatus.NO_CONTENT);
+        } catch (ResourceNotFoundException e) {
+            Body body = new Body();
+            body.setCode(HttpStatus.NOT_FOUND.value());
+            body.setMessage(e.getMessage());
+
+            return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            Body body = new Body();
+            body.setCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            body.setMessage("Error deleting Bill");
+
+            return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
 
 
+    }
 }
--- src/main/java/com/apress/BankingApi/Services/BillService.java
+++ src/main/java/com/apress/BankingApi/Services/BillService.java
@@ -1,5 +1,6 @@
 package com.apress.BankingApi.Services;
 
+import com.apress.BankingApi.Exception.CustomerNotFoundException;
 import com.apress.BankingApi.Models.Bill;
 import com.apress.BankingApi.Repos.BillRepository;
 import jakarta.persistence.PostUpdate;
@@ -11,28 +12,35 @@ import java.util.Optional;
 public class BillService {
     @Autowired
     private BillRepository billRepository;
-    public void savePoll(Bill bill)
+    public void saveBill(Bill bill)
     {
         billRepository.save(bill);
     }
     public Iterable<Bill> getAllBills (){
         return billRepository.findAll();
     }
+    public Bill createBill(Bill bill) throws CustomerNotFoundException {
+
+       // logger.info("successfully created Customer");
+        return billRepository.save(bill);
+
+    }
 
     public void deleteBillById(Long id) {
         billRepository.deleteById(id);
     }
 
-    public Optional<Bill> getPollById(Long id){
+    public Optional<Bill> getBillById(Long id){
         return billRepository.findById(id);
     }
 
     @PostUpdate
-    public void updatePoll(Bill bill, Long id){
+    public Bill updateBill(Bill bill, Long id){
         for(Bill b : getAllBills()) {
             if (b.getId() == (id)) {
                 billRepository.save(bill);
             }
         }
+        return bill;
     }
 }
